45d5a71cb249f2db7569e52b87435dfb4f92f39e
Adrien Leger <adrien.leger@inserm.fr>
2015-05-27 17:24:26 +0200

    Split sequence into 2 subclasses masker vs replacer

--- src/Sequence.py
+++ src/Sequence.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
 """
-@package    RefMasker
-@brief      Helper class for RefMasker to represent Sequences
+@package    Refeed
+@brief      Helper class for Refeed to represent Sequences
 @copyright  [GNU General Public License v2](http://www.gnu.org/licenses/gpl-2.0.html)
 @author     Adrien Leger - 2014
 * <adrien.leger@gmail.com> <adrien.leger@inserm.fr> <adrien.leger@univ-nantes.fr>
@@ -12,27 +12,23 @@
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 class Sequence(object):
-    """ Represent a single sequence from a fasta file """
+    """ Base class representing a single sequence from a fasta file """
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-
-    # Class dict to compute the complement of a DNA sequence
-    DNA_COMPLEMENT = {'A':'T','T':'A','G':'C','C':'G','N':'N','a':'t','t':'a','g':'c','c':'g','n':'n'}
 
     #~~~~~~~FUNDAMENTAL METHODS~~~~~~~#
 
-    def __init__ (self, name, seq_record, descr=""):
+    def __init__ (self, name, seq_record):
         """
         Create a Sequence object that will store
         """
         # Create self variables
         self.name = name
         self.seq_record = seq_record
-        self.descr = descr
         self.seq_len = len(self.seq_record)
 
         # Will be used later to store blast hits
         self.hit_list = []
-        self.mask_base = 0
+        self.mod_bases = 0
 
     def __str__(self):
         msg = "SEQUENCE CLASS\tParameters list\n"
@@ -56,10 +52,108 @@ class Sequence(object):
         """Support for len method"""
         return len(self.seq_record)
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+class Sequence_with_masker(Sequence):
+    """
+    Represent a single sequence from a fasta file and can store a list of Blasthits found by Blastn
+    Support masking of the original sequence with the *N* where blast hits were found
+    """
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
     #~~~~~~~PUBLIC METHODS~~~~~~~#
 
     def add_hit (self, hit):
-        """Add a hit to hit_list after verification and eventual modifications"""
+        """
+        Add a hit to hit_list after verification and eventual modifications depending of the
+        subject and query orientation
+        """
+        # Hit cannot have borders outside the sequence size
+        if hit.s_start > self.seq_len or hit.s_end > self.seq_len:
+            raise ValueError, ("Invalid hit: Outside of sequence borders")
+        if hit.s_id != self.name:
+            print hit.s_id
+            print self.name
+            raise ValueError, ("Invalid hit: hit subject name does not match Sequence name")
+
+        # Reverse coordinates of the subject if the orientation of read is negative
+        if hit.s_orient == "-":
+            hit.s_start, hit.s_end = hit.s_end, hit.s_start
+
+        # Reverse coordinates of the query if the orientation of read is negative
+        if hit.q_orient == "-":
+            hit.q_start, hit.q_end = hit.q_end, hit.q_start
+
+        # Finally append the modified hit to the list
+        self.hit_list.append(hit)
+
+    def output_sequence (self):
+        """
+        Output a sequence corresponding to the original seq record sequence but masked with
+        a masking character for bases overlapped by a BlastHit
+        """
+
+        if not self.hit_list:
+            # No need to modify the sequence
+            return str(self.seq_record)
+
+        # init a STR buffer to create the masked sequence
+        masked_seq = ""
+        # Sort list by hit subject start position
+        self.hit_list.sort(key=lambda x: x.s_start)
+
+        # Head of the hit_list. If the first hit do not start at the beginning of the subject
+        # keep the original sequence until the first hit and save start and end of the first hit
+        first_hit = self.hit_list[0]
+        start_mask, end_mask = first_hit.s_start, first_hit.s_end
+        if first_hit.s_start > 0:
+            masked_seq += str(self.seq_record[0:first_hit.s_start])
+
+        for hit in self.hit_list[1:self.n_hit]:
+            if hit.s_start <= end_mask+1:
+                if hit.s_end > end_mask:
+                    end_mask = hit.s_end
+
+            else:
+                # Write previous masked interval and update the counter of masked bases
+                masked_seq += "N"*(end_mask-start_mask)
+                self.mod_bases += end_mask-start_mask
+                # Write current sequence interval
+                masked_seq += str(self.seq_record[end_mask:hit.s_start])
+                # Update start_mask and end_mask borders
+                start_mask, end_mask = hit.s_start, hit.s_end
+
+        # Tail of the list. Write previous masked interval and update the counter of masked bases
+        masked_seq += "N"*(end_mask-start_mask)
+        self.mod_bases += end_mask-start_mask
+        # Write the end of sequence if there is something left to write
+        masked_seq += str(self.seq_record[end_mask:self.seq_len])
+
+        return masked_seq
+
+    def get_report (self):
+        """Generate a report under the form of a list"""
+        pass
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+class Sequence_with_replacer(Sequence):
+    """
+    Represent a single sequence from a fasta file and can store a list of Blasthits found by Blastn
+    Support replacement of the original sequence with the sequence of hits where blast hits were
+    found
+    """
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+    # Class dict to compute the complement of a DNA sequence
+    DNA_COMPLEMENT = {'A':'T','T':'A','G':'C','C':'G','N':'N','a':'t','t':'a','g':'c','c':'g','n':'n'}
+
+    #~~~~~~~PUBLIC METHODS~~~~~~~#
+
+    def add_hit (self, hit):
+        """
+        Add a hit to hit_list after verification and eventual modifications depending of the
+        subject and query orientation and reversing of the aligned query sequence if needed
+        """
 
         # Hit cannot have borders outside the sequence size
         if hit.s_start > self.seq_len or hit.s_end > self.seq_len:
@@ -96,62 +190,14 @@ class Sequence(object):
         # Finally append the modified hit to the list
         self.hit_list.append(hit)
 
-    def output_masked_sequence (self):
+    ###### TODO
+    def output_sequence (self):
         """
-        Output a sequence corresponding to the original seq record sequence but masked with
-        a masking character for bases overlapped by a BlastHit
+        Output a sequence corresponding to the original seq record sequence but replaced with
+        the sequence of hits where blast hits were found
         """
-
-        ######################################### TODO = Count the number of masked bases
-
-        if not self.hit_list:
-            # No need to modify the sequence
-            return str(self.seq_record)
-
-        # init a STR buffer to create the masked sequence
-        masked_seq = ""
-        # Sort list by hit subject start position
-        self.hit_list.sort(key=lambda x: x.s_start)
-
-        # Head of the hit_list. If the first hit do not start at the beginning of the subject
-        # keep the original sequence until the first hit and save start and end of the first hit
-        first_hit = self.hit_list[0]
-        start_mask, end_mask = first_hit.s_start, first_hit.s_end
-        if first_hit.s_start > 0:
-            masked_seq += str(self.seq_record[0:first_hit.s_start])
-
-        for hit in self.hit_list[1:self.n_hit]:
-            if hit.s_start <= end_mask+1:
-                if hit.s_end > end_mask:
-                    end_mask = hit.s_end
-
-            else:
-                # Write previous masked interval
-                masked_seq += "N"*(end_mask-start_mask)
-                # Write current sequence interval
-                masked_seq += str(self.seq_record[end_mask:hit.s_start])
-                # Update start_mask and end_mask borders
-                start_mask, end_mask = hit.s_start, hit.s_end
-
-        # Tail of the hit_list. Write previous masked interval and write the end of sequence if
-        # there is something left to write
-        masked_seq += "N"*(end_mask-start_mask)
-        masked_seq += str(self.seq_record[end_mask:self.seq_len])
-
-        return masked_seq
-
-    #def output_replaced_sequence (self):
-        #"""Add a hit to hit_list after verification and eventual modifications"""
-        #if not self.hit_list:
-            #return None # No need to modify the sequence
-
-        ## init a STR buffer to create the masked sequence
-        #Masked_seq = ""
-        ## Sort list by hit subject start position
-        #self.hit_list.sort(key=lambda x: x.s_start)
-
+        pass
         ## CREATE META HITS... I DON'T KNOW HOW ... TAKE INTO ACCOUNT ILLEGAL CHARACTERS...
-
 
     def get_report (self):
         """Generate a report under the form of a list"""
